86a08bbd002c5f3ac2c69e9af7a88fc79ead808e
Imani Dap <imani.dap@gmail.com>
2018-07-17 21:27:36 +0200

    pass argument to light script

--- server.js
+++ server.js
@@ -17,16 +17,22 @@ app.get('/', (req, res) => {
 
 //I know this is not the best way but the max size is 4.29 billion elements so with a restart every 3 hours it should be alright
 let history = [{time: 0}]; 
+let lightIndex = -1;
 
 io.on('connection', (socket) => {
     console.log('new connection') 
 
     socket.on('noteRequest', (data) => {
         let touch = data.payload
+        lightIndex = lightIndex++
+
+        if (lightIndex > 5) {
+            lightIndex = 0
+        }
 
         if(touch.time - history[history.length - 1].time > 510) {
             console.log('touch')
-            const lights = spawn('python3', ['./lights.py'])
+            const lights = spawn('python3', ['./lights.py', lightIndex])
             lights.stdout.on('data', (out) => {
                 console.log('[lights] ' + out.toString('utf8'))
             })
